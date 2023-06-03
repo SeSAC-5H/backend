9093e6645ff64ddba0e7459a58d7d3a05ed5b656
yamkant <dev.yamkim@gmail.com>
2023-06-03 23:53:08 +0900

    feat: #1 [Setting: deploy] 배포 관련 설정 추가

---
+++ web/apps/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+python3 -m pip install --upgrade pip
+pip install -r requirements.txt
+
+python3 manage.py collectstatic
+
+# python manage.py migrate
+
+gunicorn --bind 0.0.0.0:8000 config.wsgi:application
