301510c0a0e91a7fe5668f6aeb22d2ce3dd214e4
yamkant <dev.yamkim@gmail.com>
2023-06-05 19:46:51 +0900

    feat: #6 [Setting: migration] remote mysql 설정 및 환경변수 예시 추가

--- web/apps/config/settings/development.py
+++ web/apps/config/settings/development.py
@@ -9,9 +9,27 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get("DB_ENGINE"),
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT"),
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'use_unicode': True,
+        },
+        'TEST': {
+            'NAME': os.environ.get("TEST_DB_NAME"),
+        }
     }
 }
