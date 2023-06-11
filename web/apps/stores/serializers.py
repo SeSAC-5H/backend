3fb6870a7ee9c3ca424d9b495fa2b4bc07b8b0ad
hyerimkim05 <hyerimkim@cryptolab.co.kr>
2023-06-11 14:53:39 +0900

    add: 상점 제보 기능 추가

--- web/apps/stores/serializers.py
+++ web/apps/stores/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from core.serializers import CreateSerializer
-from stores.models import Stores
+from stores.models import Stores, Report
 
 
 class StoreSerializer(serializers.ModelSerializer):
@@ -31,3 +31,24 @@ class StoreCreateSerializer(CreateSerializer):
 
     def create(self, validated_data):
         return Stores.objects.create(**validated_data)
+
+
+class ReportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Report
+        exclude = ["updated_at", "created_at"]
+
+
+class ReportCreateSerializer(CreateSerializer):
+    representation_serializer_class = StoreSerializer
+
+    class Meta:
+        model = Report
+        fields = [
+            "report_name",
+            "report_address",
+            "report_tel",
+        ]
+
+    def create(self, validated_data):
+        return Report.objects.create(**validated_data)
