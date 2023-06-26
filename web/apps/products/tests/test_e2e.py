6c53c964b8da4b906e2221743974a5522b73f734
yamkant <dev.yamkim@gmail.com>
2023-06-27 08:25:57 +0900

    feat: #51 [Test: products] 해시태그 생성 관련 테스터 추가

--- web/apps/products/tests/test_e2e.py
+++ web/apps/products/tests/test_e2e.py
@@ -180,3 +180,109 @@ class BrandCreateAPIViewTest(E2EBaseTestCase):
             expectedStatusCode=400,
             authUser=self.user,
         )
+
+class HashtagModelViewSetTest(E2EBaseTestCase):
+    url = reverse_lazy('hashtags:list-create')
+
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.user = cls.createUser()
+
+        cls.hashQ1 = Hashtag.objects.create(
+            hash_name='칫솔',
+            hash_avg_price=3000,
+            room_type='RT11'
+        )
+        cls.hashQ2 = Hashtag.objects.create(
+            hash_name='치약',
+            hash_avg_price=2000,
+            room_type='RT12'
+        )
+
+    def test_success_list_FindStartswith(self):
+        endPoint = self.url + '?room_type=RT1'
+        res = self.generic_test(
+            url=endPoint,
+            method="get",
+            expectedStatusCode=200,
+        )
+        self.assertEqual(len(res.data), 2)
+
+    def test_success_list_FindExactOne(self):
+        endPoint = self.url + f'?room_type={self.hashQ1.room_type}'
+        res = self.generic_test(
+            url=endPoint,
+            method="get",
+            expectedStatusCode=200,
+        )
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]['hash_name'], self.hashQ1.hash_name)
+
+    def test_failure_list_WithoutQueryParam(self):
+        endPoint = self.url
+        res = self.generic_test(
+            url=endPoint,
+            method="get",
+            expectedStatusCode=200,
+        )
+        self.assertEqual(len(res.data), 0)
+
+    def test_success_create(self):
+        endPoint = self.url
+        res = self.generic_test(
+            url=endPoint,
+            method="post",
+            expectedStatusCode=201,
+            authUser=self.user,
+            hash_name="칫솔케이스",
+            room_type='RT13'
+        )
+
+    def test_failure_create_WithoutAuthUser(self):
+        endPoint = self.url
+        res = self.generic_test(
+            url=endPoint,
+            method="post",
+            expectedStatusCode=401,
+            hash_name="칫솔케이스",
+            room_type='RT13'
+        )
+
+    def test_failure_create_WithoutHashName(self):
+        endPoint = self.url
+        res = self.generic_test(
+            url=endPoint,
+            method="post",
+            expectedStatusCode=400,
+            authUser=self.user,
+            room_type='RT13'
+        )
+
+    def test_failure_create_WithoutRoomType(self):
+        endPoint = self.url
+        res = self.generic_test(
+            url=endPoint,
+            method="post",
+            expectedStatusCode=400,
+            authUser=self.user,
+            hash_name="칫솔케이스",
+        )
+
+    def test_failure_create_NotUniqueHashName(self):
+        endPoint = self.url
+        res = self.generic_test(
+            url=endPoint,
+            method="post",
+            expectedStatusCode=201,
+            authUser=self.user,
+            hash_name="칫솔케이스",
+            room_type='RT13'
+        )
+        res = self.generic_test(
+            url=endPoint,
+            method="post",
+            expectedStatusCode=400,
+            authUser=self.user,
+            hash_name="칫솔케이스",
+            room_type='RT14'
+        )
