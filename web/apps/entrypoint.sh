a92331e5fcd1a44d7bdcb0a985be6c525509ecaf
yamkant <dev.yamkim@gmail.com>
2023-06-29 22:32:44 +0900

    feat: [Setting] 배포환경 설정 수정

--- web/apps/entrypoint.sh
+++ web/apps/entrypoint.sh
@@ -3,10 +3,8 @@
 python3 -m pip install --upgrade pip
 pip install -r requirements.txt
 
-python3 manage.py collectstatic
+#python3 manage.py collectstatic
 
 # python manage.py migrate
 
-python3 manage.py runserver 0.0.0.0:8000
-
-# gunicorn --bind 0.0.0.0:8000 config.wsgi:application
+gunicorn --bind 0.0.0.0:8000 config.wsgi:application
